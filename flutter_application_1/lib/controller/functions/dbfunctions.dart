975d670ee5f5c65db7468a67f03b7f8e9057e658
adinanhamza <adinanck4557@gmail.com>
2025-03-06 15:05:47 +0530

    new page created

--- flutter_application_1/lib/controller/functions/dbfunctions.dart
+++ flutter_application_1/lib/controller/functions/dbfunctions.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/view/editstudent.dart';
 import 'package:flutter_application_1/model/model.dart';
 import 'package:hive_flutter/adapters.dart';
 
